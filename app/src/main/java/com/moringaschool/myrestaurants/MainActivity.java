93c37570703f1959b1b08817edf4e62784c231aa
Brian Marete <bmarete10@gmail.com>
2019-09-24 16:46:33 +0300

    Get input from user and display on logcat

--- app/src/main/java/com/moringaschool/myrestaurants/MainActivity.java
+++ app/src/main/java/com/moringaschool/myrestaurants/MainActivity.java
@@ -4,22 +4,31 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String TAG = MainActivity.class.getSimpleName();
+
     private Button mFindRestaurantsButton;
+    private EditText mLocationEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mLocationEditText = findViewById(R.id.locationEditText);
+
         mFindRestaurantsButton = findViewById(R.id.findRestaurantsButton);
         mFindRestaurantsButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                String location = mLocationEditText.getText().toString();
+                Log.d(TAG, location);
+
                 Intent intent = new Intent(MainActivity.this, RestaurantsActivity.class);
                 startActivity(intent);
             }
