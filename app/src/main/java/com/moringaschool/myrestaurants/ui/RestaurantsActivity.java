dae281b0fa0f50d3549be8b635181ebdf8b54ea6
Brian Marete <bmarete10@gmail.com>
2019-10-08 14:25:43 +0300

    Remove unused imports

--- app/src/main/java/com/moringaschool/myrestaurants/ui/RestaurantsActivity.java
+++ app/src/main/java/com/moringaschool/myrestaurants/ui/RestaurantsActivity.java
@@ -7,23 +7,16 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import com.moringaschool.myrestaurants.MyRestaurantsArrayAdapter;
 import com.moringaschool.myrestaurants.R;
 import com.moringaschool.myrestaurants.adapters.RestaurantListAdapter;
 import com.moringaschool.myrestaurants.models.Business;
-import com.moringaschool.myrestaurants.models.Category;
 import com.moringaschool.myrestaurants.models.YelpBusinessesSearchResponse;
 import com.moringaschool.myrestaurants.network.YelpApi;
 import com.moringaschool.myrestaurants.network.YelpClient;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import butterknife.BindView;
